c749f4bc465f34d20886055890bf7f62e9802118
Alex Resovsky <45566769+hellonskis@users.noreply.github.com>
2021-03-26 16:40:53 +0100

    Update function_list.py

--- function_list.py
+++ function_list.py
@@ -185,8 +185,133 @@ def to_precision(x,p):
 ## https://github.com/randlet/to-precision/blob/master/to_precision.py
     
 ###########################################################################################
-## A simple implementation of the LOESS algorithm using numpy based on NIST.   
+## Function to calculate envelope bounds for anomaly classification.
+## In the main code, we calculate dC, the daily difference between the CCGcrv smoothed, detrended annual cycle 
+## curve and the CCGcrv harmonic curve. The envelope bounds are then defined for each day, t, using the standard 
+## deviation of all dC values within +/- 45 days of the calendar date of t over all years in the record. 
+def seasonal_sigma(df, ndays, dt_col):
+    sd = np.empty([0, 1])
+    rs = np.empty([0, 1])    
+    for i in range((len(df)-ndays),len(df)):
+        i_percentage(i, df, ndays)
+        if (df[dt_col].iloc[i].timetuple().tm_yday >= 46 and 
+                df[dt_col].iloc[i].timetuple().tm_yday <= 320): 
+            for j in range(0,len(df)):
+                if (df[dt_col].iloc[j].timetuple().tm_yday >= (df[dt_col].iloc[i].timetuple().tm_yday - 45) and 
+                        df[dt_col].iloc[j].timetuple().tm_yday <= (df[dt_col].iloc[i].timetuple().tm_yday + 45)):
+                    rs = np.append(rs, df['dC'].iloc[j])
+            std = np.std(rs)
+            sd = np.append(sd, std)
+            rs = np.empty([0, 1])
+        elif df[dt_col].iloc[i].timetuple().tm_yday < 46:
+            for j in range(0,len(df)):
+                if (df[dt_col].iloc[j].timetuple().tm_yday <= df[dt_col].iloc[i].timetuple().tm_yday or 
+                        df[dt_col].iloc[j].timetuple().tm_yday >= (df[dt_col].iloc[i].timetuple().tm_yday + 320) or
+                        df[dt_col].iloc[j].timetuple().tm_yday <= (df[dt_col].iloc[i].timetuple().tm_yday + 45)):
+                    rs = np.append(rs, df['dC'].iloc[j])
+            std = np.std(rs)
+            sd = np.append(sd, std)
+            rs = np.empty([0, 1])
+        elif df[dt_col].iloc[i].timetuple().tm_yday > 320:
+            for j in range(0,len(df)):
+                if (df[dt_col].iloc[j].timetuple().tm_yday >= df[dt_col].iloc[i].timetuple().tm_yday or 
+                        df[dt_col].iloc[j].timetuple().tm_yday <= (df[dt_col].iloc[i].timetuple().tm_yday + 320) or
+                        df[dt_col].iloc[j].timetuple().tm_yday >= (df[dt_col].iloc[i].timetuple().tm_yday - 45)):
+                    rs = np.append(rs, df['dC'].iloc[j])
+            std = np.std(rs)
+            sd = np.append(sd, std)
+            
+    return(sd)
 
+
+def seasonal_sigma_noleap(df, ndays, dt_col):
+    sd = np.empty([0, 1])
+    rs = np.empty([0, 1])    
+    assign_sd = False
+    for i in range((len(df)-ndays),len(df)):
+        year_percentage(i, df, ndays)
+        if i == (len(df)-ndays):
+            month = df[dt_col].iloc[i].month
+            day = df[dt_col].iloc[i].day
+        if ((df[dt_col].iloc[i].month == month) and (df[dt_col].iloc[i].day == day) and 
+            (i != (len(df)-ndays))):
+            assign_sd = True
+            print ("1 year complete, assigning sigma values")
+            for j in range(i, len(df)):
+                std = sd[j-365]
+                sd = np.append(sd, std)
+        if bool(assign_sd):
+          print("done")
+          break
+        if (df[dt_col].iloc[i].timetuple().tm_yday >= 46 and 
+                df[dt_col].iloc[i].timetuple().tm_yday <= 320): 
+            for j in range(0,len(df)):
+                if (df[dt_col].iloc[j].timetuple().tm_yday >= (df[dt_col].iloc[i].timetuple().tm_yday - 45) and 
+                        df[dt_col].iloc[j].timetuple().tm_yday <= (df[dt_col].iloc[i].timetuple().tm_yday + 45)):
+                    rs = np.append(rs, df['dC'].iloc[j])
+            std = np.std(rs)
+            sd = np.append(sd, std)
+            rs = np.empty([0, 1])
+        elif df[dt_col].iloc[i].timetuple().tm_yday < 46:
+            for j in range(0,len(df)):
+                if (df[dt_col].iloc[j].timetuple().tm_yday <= df[dt_col].iloc[i].timetuple().tm_yday or 
+                        df[dt_col].iloc[j].timetuple().tm_yday >= (df[dt_col].iloc[i].timetuple().tm_yday + 320) or
+                        df[dt_col].iloc[j].timetuple().tm_yday <= (df[dt_col].iloc[i].timetuple().tm_yday + 45)):
+                    rs = np.append(rs, df['dC'].iloc[j])
+            std = np.std(rs)
+            sd = np.append(sd, std)
+            rs = np.empty([0, 1])
+        elif df[dt_col].iloc[i].timetuple().tm_yday > 320:
+            for j in range(0,len(df)):
+                if (df[dt_col].iloc[j].timetuple().tm_yday >= df[dt_col].iloc[i].timetuple().tm_yday or 
+                        df[dt_col].iloc[j].timetuple().tm_yday <= (df[dt_col].iloc[i].timetuple().tm_yday + 320) or
+                        df[dt_col].iloc[j].timetuple().tm_yday >= (df[dt_col].iloc[i].timetuple().tm_yday - 45)):
+                    rs = np.append(rs, df['dC'].iloc[j])
+            std = np.std(rs)
+            sd = np.append(sd, std)
+            
+    return(sd)
+
+            
+#------------------------------------------------------------  
+def i_percentage(i, df, ndays):
+    percentage = np.linspace(5, 100, 20)
+    for p in percentage:
+        if i - (len(df) - ndays) == round(ndays/100 * p):
+            print(p, "% complete")
+            
+            
+#------------------------------------------------------------
+def year_percentage(i, df, ndays):   
+    percentage = np.linspace(10, 100, 10)   
+    for p in percentage:
+        if ((round((i/365)*100) == p) and (round(((i-1)/365)*100) == (p-1))):
+            print(p, "% complete")
+            
+#------------------------------------------------------------            
+## Author: Alex Resovsky
+
+###########################################################################################
+## Function to fill gaps in seasonal and synoptic anomaly plots 
+def plot_clean(df, pos, neg, none):
+    
+    for row in df.iloc[0:(len(df)-1)].itertuples():
+        if ((math.isnan(df[pos].iat[row.Index]) & math.isnan(df[none].iat[row.Index + 1]) 
+           & math.isnan(df[neg].iat[row.Index + 1])) | 
+           (math.isnan(df[pos].iat[row.Index]) & math.isnan(df[none].iat[row.Index - 1]) 
+           & math.isnan(df[neg].iat[row.Index - 1]))):
+               df[pos].iat[row.Index] = 0
+        elif ((math.isnan(df[neg].iat[row.Index]) & math.isnan(df[none].iat[row.Index + 1]) 
+             & math.isnan(df[pos].iat[row.Index + 1])) |  
+             (math.isnan(df[neg].iat[row.Index]) & math.isnan(df[none].iat[row.Index - 1]) 
+             & math.isnan(df[pos].iat[row.Index - 1]))):
+               df[neg].iat[row.Index] = 0 
+    
+#------------------------------------------------------------            
+## Author: Alex Resovsky
+
+###########################################################################################
+## A simple implementation of the LOESS algorithm using numpy based on NIST.   
 def tricubic(x):
     y = np.zeros_like(x)
     idx = (x >= -1) & (x <= 1)
